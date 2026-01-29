231aa85f4e76bc3fd34d3358190b55a84d3c402a
reydeoscuro <68565500+reydeoscuro@users.noreply.github.com>
2026-01-29 15:51:06 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,24 @@
-console.log("APP.JS CARGADO CORRECTAMENTE");
+console.log("APP.JS CARGADO CON API");
 
-const contenedor = document.getElementById("productos");
+const URL = "Phttps://sheetdb.io/api/v1/1unfoggs799r3";
 
-contenedor.innerHTML = "<h2>JS conectado correctamente</h2>";
+fetch(URL)
+  .then(response => response.json())
+  .then(productos => {
+    const contenedor = document.getElementById("productos");
+    contenedor.innerHTML = ""; // limpia
 
+    productos.forEach(p => {
+      contenedor.innerHTML += `
+        <div>
+          <h3>${p.nombre}</h3>
+          <p>Precio: $${p.precio}</p>
+          <p>Stock: ${p.stock}</p>
+        </div>
+        <hr>
+      `;
+    });
+  })
+  .catch(error => {
+    console.error("ERROR FETCH:", error);
+  });
