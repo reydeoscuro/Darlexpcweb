4e24f2eb1cc5773cee0a38842c03845ec36b56fd
reydeoscuro <68565500+reydeoscuro@users.noreply.github.com>
2026-02-04 00:19:32 -0500

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,1 +1,12 @@
+let index = 0;
+const track = document.querySelector('.slider-track');
+const slides = document.querySelectorAll('.slide');
+
+function moveSlider() {
+  index++;
+  if (index >= slides.length) index = 0;
+  track.style.transform = `translateX(-${index * 100}%)`;
+}
+
+setInterval(moveSlider, 4000);
 
