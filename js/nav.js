9cabb512cfa636836517ec8d2e111658572aeb59
reydeoscuro <68565500+reydeoscuro@users.noreply.github.com>
2026-02-10 19:38:58 -0500

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -4,20 +4,8 @@ console.log("nav.js cargado");
 const menu = document.querySelector(".menu");
 if (!menu) return;
 
-// hamburguesa
-const menuToggle = document.getElementById("menuToggle");
-const menuItems = document.getElementById("menuItems");
-
-if (menuToggle && menuItems) {
-  menuToggle.addEventListener("click", () => {
-    menuItems.classList.toggle("active");
-  });
-}
-
-// filtro categorías
-const botonesCategoria = document.querySelectorAll(".menu-items button");
-
-botonesCategoria.forEach(btn => {
+// filtro por categoría
+menu.querySelectorAll("button").forEach(btn => {
   btn.addEventListener("click", () => {
     const categoria = btn.dataset.cat;
 
@@ -25,8 +13,5 @@ botonesCategoria.forEach(btn => {
       card.style.display =
         card.dataset.cat === categoria ? "block" : "none";
     });
-
-    // cerrar menú móvil
-    if (menuItems) menuItems.classList.remove("active");
   });
 });
